38d7a4274c39ef75263e4558705812b56c0b6975
Fabian Vergara <vergarf@gmail.com>
2016-05-13 21:02:04 -0400

    Fixed keyboard bug.

--- Is It Prime/ViewController.swift
+++ Is It Prime/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
     
     @IBOutlet var userInput: UITextField!
     @IBOutlet var resultLabel: UILabel!
@@ -49,12 +49,24 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        self.userInput.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool{
+        textField.resignFirstResponder()
+        return true
+    }
+    
 
 
 }
